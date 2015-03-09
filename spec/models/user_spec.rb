2483a8cee5a0960b756d1fbce934ae006f61b6ae
adamberman <adamberman13@gmail.com>
2015-03-09 15:10:12 -0700

    write first tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  pending "hello"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to :name }
+  it { should respond_to :email }
 end
