62a463023d7b278bc5ba257854762963903eb184
adamberman <adamberman13@gmail.com>
2015-03-09 12:58:50 -0700

    Revert "add factorygirl, final test setup features"
    This reverts commit bf0e34109ee5105c8573c3cfd2ae768f5ff89d31.
    
    revert devise commits

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module Vouch
         :routing_specs => false,
         :controller_specs => true,
         :request_specs => true
-      g.fixture_replacement :factory_girl,
-        :dir => "spec/factories"
     end
   end
 end
